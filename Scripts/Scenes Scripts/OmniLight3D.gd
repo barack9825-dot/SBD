fe39dee3ca2999d12660a62713fecb01712ba41d
Rubén David Pérez-Borroto Chaveco <rupedaveco25@gmail.com>
2026-01-02 05:03:37 -0600

    Arreglado el problema de la segunda luz

--- Scripts/Scenes Scripts/OmniLight3D.gd
+++ Scripts/Scenes Scripts/OmniLight3D.gd
@@ -7,12 +7,9 @@ func _on_player_intensity(I,id):
 		if I == 'z':
 			light_energy += 5 * get_process_delta_time()
 
-
-
 func _on_omni_light_area_body_entered(body):
 	if body.is_in_group("Enemy"):
 		body.enter_omni_light_area(self)
-
 
 func _on_omni_light_area_body_exited(body):
 	if body.is_in_group("Enemy"):
